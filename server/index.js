c5eed8e843b9c2832d643f0f687d01f911539178
Jorge Sanchez <jorgesinsilla@gmail.com>
2023-05-01 16:21:30 +0100

    do user routes setup

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ import morgan from "morgan";
 import path from "path"; //https://nodejs.org/api/path.html#path
 import { fileURLToPath } from "url"; //https://nodejs.org/api/url.html#urlfileurltopathurl
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
-import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -43,7 +43,8 @@ const upload = multer({ storage });
 app.post("auth/register", upload.single("picture"), register);
 
 /* ROUTES */
-app.use("/auth", authRoutes)
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 /*MONGOOSE SETUP*/
 const PORT = process.env.PORT || 6001;
