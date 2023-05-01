867849ca26194979760f2b0f647b7c8166eb5c93
Jorge Sanchez <jorgesinsilla@gmail.com>
2023-05-01 15:56:51 +0100

    create authorization & verification & add images

--- server/index.js
+++ server/index.js
@@ -10,7 +10,9 @@ import morgan from "morgan";
 //native packages from node
 import path from "path"; //https://nodejs.org/api/path.html#path
 import { fileURLToPath } from "url"; //https://nodejs.org/api/url.html#urlfileurltopathurl
-import {register} from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -40,6 +42,8 @@ const upload = multer({ storage });
 /* ROUTES WITH FILES */
 app.post("auth/register", upload.single("picture"), register);
 
+/* ROUTES */
+app.use("/auth", authRoutes)
 
 /*MONGOOSE SETUP*/
 const PORT = process.env.PORT || 6001;
