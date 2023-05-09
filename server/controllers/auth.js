9aa8702cc77263c3f6cf40e786d46909c8800610
Jorge Sanchez <jorgesinsilla@gmail.com>
2023-05-09 19:08:20 +0100

    create navbar

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -38,7 +38,7 @@ export const register = async (req, res) => {
   }
 };
 
-/*  LOGGIN IN */
+/*  LOGIN IN */
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
