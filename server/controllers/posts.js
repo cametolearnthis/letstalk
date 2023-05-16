080a96f5f3b7e6794d547615c3b763acf0e58380
Jorge Sanchez <jorgesinsilla@gmail.com>
2023-05-16 17:42:51 +0100

    create mypostwidget

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -12,12 +12,13 @@ export const createPost = async (req, res) => {
       lastName: user.lastName,
       location: user.location,
       description,
-      userPicturePath: user.userPicturePath,
+      userPicturePath: user.picturePath,
       picturePath,
       likes: {},
       comments: [],
     });
     await newPost.save();
+
     const post = await Post.find();
     res.status(201).json(post);
   } catch (err) {
@@ -52,18 +53,20 @@ export const likePost = async (req, res) => {
     const { userId } = req.body;
     const post = await Post.findById(id);
     const isLiked = post.likes.get(userId);
-    
-    if(isLiked) {
-        post.likes.delete(userId)
+
+    if (isLiked) {
+      post.likes.delete(userId);
     } else {
-        post.likes.set(userId, true)
+      post.likes.set(userId, true);
     }
+
     const updatedPost = await Post.findByIdAndUpdate(
-        id,
-        { likes: post.likes },
-        { new: true}
-    )
-    res.status(200).json(post);
+      id,
+      { likes: post.likes },
+      { new: true }
+    );
+
+    res.status(200).json(updatedPost);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
